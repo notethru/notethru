f9c7217301caca66edd38a675221f7554f3f3b96
Danish <hypertxt.assin@gmail.com>
2023-06-04 13:35:23 +0530

    some useful changes

--- packages/create-notethru-component/createNotethruComponent.js
+++ packages/create-notethru-component/createNotethruComponent.js
@@ -2,27 +2,11 @@
 //   /!\ DO NOT MODIFY THIS FILE /!\
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 //
-// The only job of create-react-app is to init the repository and then
-// forward all the commands to the local version of create-react-app.
-//
-// If you need to add a new command, please add it to the scripts/ folder.
-//
-// The only reason to modify this file is to add more warnings and
-// troubleshooting information for the `create-react-app` command.
-//
-// Do not make breaking changes! We absolutely don't want to have to
-// tell people to update their global version of create-react-app.
-//
-// Also be careful with new language features.
-// This file must work on Node 10+.
-//
-// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-//   /!\ DO NOT MODIFY THIS FILE /!\
-// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
+// This file will be stored in user's computer forever and therefore,
+// We can't ask users to keep updating there package create-notethru component again
+// and again
 'use strict';
 
-const https = require('https');
 const chalk = require('chalk');
 const commander = require('commander');
 const dns = require('dns');
