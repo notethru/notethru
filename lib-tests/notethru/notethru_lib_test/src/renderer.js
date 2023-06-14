51b6f533ed0f9fe6b63a120e5c48b76e21a6242d
Danish <hypertxt.assin@gmail.com>
2023-06-14 13:29:15 +0530

    Work on Publish Command

--- lib-tests/notethru/notethru_lib_test/src/renderer.js
+++ lib-tests/notethru/notethru_lib_test/src/renderer.js
@@ -2,12 +2,12 @@
  * THE APPLICATION TO WORK PROPERLY ++
  */
 
-import NotethruComponent from "./Component"
+import Component from "./Component"
 import React from "react"
 import { createRoot } from "react-dom/client"
 
-// **MAIN RENDER FUNCTION BEING USED FOR DEVELOPMENT** 
+// **MAIN RENDERER METHOD BEING USED FOR DEVELOPMENT** 
 //              **|| PLEASE DON'T TOUCH ||**
 
 const root = createRoot(document.getElementById("root"))
-root.render(<NotethruComponent />)
+root.render(<Component />)
